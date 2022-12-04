c686908f6b01043432a63bf3be837bdcb6140bf5
Ashish <ashis@LAPTOP-Q29TNCTI.(none)>
2022-12-05 00:12:34 +0530

    first commit

---
+++ myproject/myapp/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class FeedbackForm(forms.Form):
+    title = forms.CharField(label='Title',min_length=1,widget=forms.TextInput(attrs={'class':'form-control'}))
+    subject = forms.CharField(label='Subject Description',min_length=1,widget=forms.Textarea(attrs={'class':'form-control'}))
+    
